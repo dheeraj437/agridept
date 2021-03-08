cad7d167c7be12d34755e0dc17dcae8004eaac6b
dheeraj437 <37209186+dheeraj437@users.noreply.github.com>
2021-03-08 22:52:11 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,1 +1,8 @@
 
+<?php  
+       $servername = "localhost";  
+       $username = "admin";  
+       $password = "Dheeraj437@";  
+       $conn = mysql_connect ($servername , $username , $password) or die("unable to connect to host");  
+       $sql = mysql_select_db ('id16333679_wp_e3b4fd538acd12198b876c0d01f509d8',$conn) or die("unable to connect to database"); 
+?>   
